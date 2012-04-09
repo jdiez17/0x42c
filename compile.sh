fb2e31fb7c64e0649eb01651431eb396368b17ef
José Manuel Díez <jose.manuel.diez@gmail.com>
2012-04-09 13:11:24 +0200

    Whoops

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@
 
 ./dasm 0x42c.dasm16 0x42c.bin
 dd if=0x42c.bin ibs=1 skip=8192 of=0x42c_trim.bin
-./dasm bootloader.dasm16 bootloader.bin
+./dasm kernel.dasm16 kernel.bin
 #cp bootloader.bin /mnt/dcpu
