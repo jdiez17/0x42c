3b4f997f484319883fbc2636c0a6619a2b733a9b
José Manuel Díez <jose.manuel.diez@gmail.com>
2012-04-09 14:15:41 +0200

    New folder structure.

--- compile.sh
+++ compile.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # programs
-./dasm openf.dasm16 openf.bin
-dd if=openf.bin ibs=1 skip=24576 of=openf_trim.bin
-./dasm echof.dasm16 echof.bin
-dd if=echof.bin ibs=1 skip=24576 of=echof_trim.bin
-./dasm cleanf.dasm16 cleanf.bin
-dd if=cleanf.bin ibs=1 skip=24576 of=cleanf_trim.bin
+./dasm src/openf.dasm16 bin/openf.bin
+dd if=bin/openf.bin ibs=1 skip=24576 of=bin/openf_trim.bin
+./dasm src/echof.dasm16 bin/echof.bin
+dd if=bin/echof.bin ibs=1 skip=24576 of=bin/echof_trim.bin
+./dasm src/cleanf.dasm16 bin/cleanf.bin
+dd if=bin/cleanf.bin ibs=1 skip=24576 of=bin/cleanf_trim.bin
 
-./dasm 0x42c.dasm16 0x42c.bin
-dd if=0x42c.bin ibs=1 skip=8192 of=0x42c_trim.bin
-./dasm kernel.dasm16 kernel.bin
+./dasm src/0x42c.dasm16 bin/0x42c.bin
+dd if=bin/0x42c.bin ibs=1 skip=8192 of=0x42c_trim.bin
+./dasm src/kernel.dasm16 kernel.bin
 cp kernel.bin /mnt/dcpu
