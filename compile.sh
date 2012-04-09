103c9fd8d08124d5f8575b18304aaffd094302c2
José Manuel Díez <jose.manuel.diez@gmail.com>
2012-04-09 14:05:14 +0200

    Added arbitrary program loading, made some of the built-in commands "programs"

--- compile.sh
+++ compile.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# programs
+./dasm openf.dasm16 openf.bin
+dd if=openf.bin ibs=1 skip=24576 of=openf_trim.bin
+./dasm echof.dasm16 echof.bin
+dd if=echof.bin ibs=1 skip=24576 of=echof_trim.bin
+./dasm cleanf.dasm16 cleanf.bin
+dd if=cleanf.bin ibs=1 skip=24576 of=cleanf_trim.bin
+
 ./dasm 0x42c.dasm16 0x42c.bin
 dd if=0x42c.bin ibs=1 skip=8192 of=0x42c_trim.bin
 ./dasm kernel.dasm16 kernel.bin
-#cp bootloader.bin /mnt/dcpu
+cp kernel.bin /mnt/dcpu
