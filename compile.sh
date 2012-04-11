1c927f22714dc48bcfe49ab7abea03199125e322
Sean M. Collins <sean@coreitpro.com>
2012-04-11 13:21:29 -0400

    Compile and link from the bin folder

--- compile.sh
+++ compile.sh
@@ -13,6 +13,6 @@ dasm src/cleanf.dasm16 bin/cleanf.bin
 dd if=bin/cleanf.bin ibs=1 skip=24576 of=bin/cleanf_trim.bin
 
 dasm src/0x42c.dasm16 bin/0x42c.bin
-dd if=bin/0x42c.bin ibs=1 skip=8192 of=0x42c_trim.bin
-dasm src/kernel.dasm16 kernel.bin
-cp kernel.bin /mnt/dcpu
+dd if=bin/0x42c.bin ibs=1 skip=8192 of=bin/0x42c_trim.bin
+cd src; dasm kernel.dasm16 ../bin/kernel.bin; cd ../
+cp bin/kernel.bin /mnt/dcpu
